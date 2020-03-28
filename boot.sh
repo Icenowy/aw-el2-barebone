dd5cf034b0f14067e5542fbadf7820f8ae551557
Icenowy Zheng <icenowy@aosc.io>
2020-03-28 17:52:08 +0800

    boot linux and start to catch CPU_ON
    Signed-off-by: Icenowy Zheng <icenowy@aosc.io>

--- boot.sh
+++ boot.sh
@@ -1,7 +1,9 @@
-sudo sunxi-fel \
+sudo sunxi-fel -v -p \
 	spl uboot/spl32_a64_lpddr3.bin \
 	write 0x4a000000 uboot/u-boot_a64.bin \
 	write 0x44000 uboot/bl31_a64_40010000.bin \
 	write 0x4fc00000 uboot/uEnv_a64.txt \
+	write 0x40080000 uboot/kernel \
+	write 0x4fa00000 uboot/sun50i-a64-pinephone-1.1.dtb \
 	write 0x40010000 el2-bb.bin \
 	reset64 0x44000
--- trapped_funcs.c
+++ trapped_funcs.c
@@ -1,8 +1,37 @@
+#include SOC_HEADER
 #include "trapped_funcs.h"
+#include "uart.h"
 
 bool wrap_psci(struct pt_regs *pt_regs)
 {
-	pt_regs->regs[0] = (uint64_t) -1;
+	if (pt_regs->regs[0] == 0xc4000003) {
+		uart_puts(SOC_UART0, "PSCI CPU_ON caught.\n");
+
+	}
+
+	asm volatile ("mov x0, %0\n"
+		      "mov x1, %1\n"
+		      "mov x2, %2\n"
+		      "mov x3, %3\n"
+		      "mov x4, %4\n"
+		      "mov x5, %5\n"
+		      "mov x6, %6\n"
+		      "mov x7, %7\n"
+		      "smc #0\n"
+		      "mov %0, x0\n"
+		      "mov %1, x1\n"
+		      "mov %2, x2\n"
+		      "mov %3, x3\n"
+		      : "+r" (pt_regs->regs[0]),
+		        "+r" (pt_regs->regs[1]),
+		        "+r" (pt_regs->regs[2]),
+		        "+r" (pt_regs->regs[3])
+		      : "r" (pt_regs->regs[4]),
+		        "r" (pt_regs->regs[5]),
+		        "r" (pt_regs->regs[6]),
+		        "r" (pt_regs->regs[7])
+		      : "x0", "x1", "x2", "x3", "x4", "x5", "x6", "x7", "cc");
+
 	pt_regs->elr += 4;
 	return true;
 }
